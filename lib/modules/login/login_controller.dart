a010a95587e7df14a3034802c970bbc2c746e916
Beatriz Rangel <raangelbeatriz@gmail.com>
2021-06-30 17:02:31 -0300

    add user_model

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pay_flow/shared/auth/auth_controller.dart';
+import 'package:pay_flow/shared/models/user_model.dart';
 
 class LoginController {
   final authController = AuthController();
@@ -12,7 +13,9 @@ class LoginController {
     );
     try {
       final response = await _googleSignIn.signIn();
-      authController.setUser(context, response);
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
       print(response);
     } catch (error) {
       authController.setUser(context, null);
